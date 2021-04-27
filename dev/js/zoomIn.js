184b3ba1c37839d96a4b3abcd90791d7574305d4
jat425 <jat425@users.noreply.github.com>
2021-04-26 21:23:22 -0400

    Change y location of city

--- dev/js/zoomIn.js
+++ dev/js/zoomIn.js
@@ -10,9 +10,9 @@ gsap.set("#Middle",{transformOrigin:"top"});
 gsap.set("#Rocket",{transformOrigin:"top"});
 gsap.set("#Bushes-Foreground",{transformOrigin:"center"});
 
-zoomTL.from("#City3",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
-    .from("#City2",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
-    .from("#City1",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
+zoomTL.from("#City3",{duration:6, scale:10,ease: "power4.out",y: 2000},"zoom")
+    .from("#City2",{duration:6, scale:10,ease: "power4.out",y: 2000},"zoom")
+    .from("#City1",{duration:6, scale:10,ease: "power4.out",y: 2000},"zoom")
     // .from("#CityBottom",{duration:5, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
     .from("#Middle",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
     .from("#Rocket",{duration:6, scale:10,ease: "power4.out",y:"+=1500"},"zoom")
