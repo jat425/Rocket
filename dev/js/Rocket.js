0d0c0ba79f3c9e5227832594226e36b26a20c58f
jat425 <jat425@users.noreply.github.com>
2021-05-03 22:17:07 -0400

    Add flames

--- dev/js/Rocket.js
+++ dev/js/Rocket.js
@@ -2,5 +2,4 @@ import { gsap } from "gsap";
 
 export let RocketTL = gsap.timeline();
 
-RocketTL.from("#Rocket",{y:"+=1000", duration:1})
-.to("#space-ship",{y:"+=320", duration:2, ease: "bounce.out"})
+RocketTL.from("#Rocket",{y:"+=1000", duration:1});
--- dev/js/liftOff.js
+++ dev/js/liftOff.js
@@ -7,7 +7,7 @@ export let liftOffTL = gsap.timeline();
 
 liftOffTL.to("#Rocket",{duration:0.15, rotation:-5, ease:"none"})
             .to("#Rocket",{duration:0.15, rotation:5, yoyo:true, repeat:5, ease:"none"})
-            .to("#Rocket",{duration:0.15, rotation:0,ease:"none"})
+            .to("#Rocket",{duration:0.15, rotation:0,ease:"none", onComplete:controlFlames})
             .to("#Rocket",{duration:3, y:"-=400", scale:.50, ease: "power3.in"},"upAndAway")
             .to("#Clouds",{duration:5, y:"+=1000", ease: "power3.in"},'upAndAway')
             .to("#Moon",{duration:5, y:"+=1000", ease: "power3.in"},'upAndAway')
@@ -16,3 +16,18 @@ liftOffTL.to("#Rocket",{duration:0.15, rotation:-5, ease:"none"})
             .to("#Middle",{duration:5, y:"+=900", ease: "power3.in"},'upAndAway')
             .to("#City-",{duration:5, y:"+=1000", ease: "power3.in"},'upAndAway')
             .from("#Space",{duration:5,alpha:0, ease: "power3.in"},'upAndAway');
+
+
+
+export let flamesTL = gsap.timeline({paused:true});
+
+flamesTL.to("#Flame1",{duration:0.35, scaleY:.25,yoyo:true, repeat:-1},"flames")
+        .to("#Flame2",{duration:0.15, scaleY:.15,yoyo:true, repeat:-1},"flames")
+        .to("#Flame3",{duration:0.10, scaleY:.25,yoyo:true, repeat:-1},"flames")
+            
+function controlFlames(){
+    gsap.set("Flame1",{display:"block"});
+    gsap.set("Flame2",{display:"block"});
+    gsap.set("Flame3",{display:"block"});
+    flamesTL.play()
+ }
